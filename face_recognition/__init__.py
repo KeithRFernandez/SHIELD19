6d2ca71ceb587d07200176529cac102506043c07
KeithRFernandez <KeithRFernandez@users.noreply.github.com>
2020-01-10 01:00:25 -0500

    Add files via upload

---
+++ face_recognition/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """keith fernandez"""
+__email__ = 'kfernandezi@policia.gob.pe'
+__version__ = '1.2.3'
+
+from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
